a75c2d0985dd0e91d1d06a978d1e6d6bce0f6a21
OGKEk9 <81781161+ishalliveforever@users.noreply.github.com>
2025-05-22 01:51:02 +0000

    Update BsvSendRequest logic and documentation

--- src/pages/requests/BsvSendRequest.tsx
+++ src/pages/requests/BsvSendRequest.tsx
@@ -157,8 +157,7 @@ export const BsvSendRequest = (props: BsvSendRequestProps) => {
       }
     } finally {
       setIsProcessing(false);
-    }    git add .
-    git commit -m "Initial commit"
+    }
   };
 
   useEffect(() => {
